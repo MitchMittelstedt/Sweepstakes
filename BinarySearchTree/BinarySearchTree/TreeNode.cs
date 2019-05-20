8341e789f86fa853265fbf0d80555b98e925220a
Mitchell Mittelstedt <mmittelstedt@icloud.com>
2019-05-20 13:32:08 -0500

    started binary search tree as its own solution/project within tthis folder

---
+++ BinarySearchTree/BinarySearchTree/TreeNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class TreeNode
+    {
+        public int data;
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+        public TreeNode(int data)
+        {
+            this.data = data;
+        }
+    }
+}
